f8fb78cc12c9287f2d7b685e74f61a8833331115
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-09-16 14:46:55 -0500

    add welcome message

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import helloRoute from "./routes/hello.js";
 import healthRoute from "./routes/health.js";
 import quotesRoute from "./routes/quotes.js";
+import welcomeRoute from "./routes/welcome.js"
 
 const app = express();
 
@@ -9,5 +10,6 @@ const app = express();
 app.use("/hello", helloRoute);
 app.use("/health", healthRoute);
 app.use("/quotes", quotesRoute);
+app.use("/",welcomeRoute)
 
 export default app;
---
+++ src/controllers/welcomeController.js
@@ -0,0 +1,3 @@
+export const getWelcomeMessage = (req, res) => {
+  res.send("Hello, Welcome to the Quotes App! Please check /quotes/random for random quotes");
+};
