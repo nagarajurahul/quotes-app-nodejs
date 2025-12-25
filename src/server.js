5be4ad94429fbdc407351c59e25bf912d574e02c
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-12-24 22:38:36 -0600

    fix: host for nodejs app deploy inside kubernetes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 
 const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://${HOST}:${PORT}`);
 });
